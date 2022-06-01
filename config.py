30c358cdd9a8568728f3c4b2f213cfa55c4cfeb2
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-01 10:50:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+
+SUDO_USER = list(
+    map(int, getenv("SUDO_USER", "").split())
+) 
