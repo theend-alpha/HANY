9dc65f7601c6aec3fa383967e6acc06847c2cf64
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-01 11:52:52 +0530

    Update start.py

--- EndAfk/start.py
+++ EndAfk/start.py
@@ -12,6 +12,7 @@ alpha = "https://te.legra.ph/file/5a8e62a134991d1b57e69.jpg"
 async def on_start(_, message: Message):
     bot_uptime = int(time.time() - boot)
     Uptime = get_readable_time(bot_uptime)
+    first_name = message.from_user.first_name
     await message.reply_photo(alpha,
         caption=f"hey {first_name} ! This is End Afk ! \n Nice to see ya here ✨💫! \n\n • Part of The End Network"
     )
