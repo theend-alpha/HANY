4d643e691bebb2e6d24cb9b72abc9bc2a43d2fe9
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-01 12:14:00 +0530

    Update start.py

--- EndAfk/start.py
+++ EndAfk/start.py
@@ -9,7 +9,9 @@ from EndAfk import SUDOERS
 
 alpha = "https://te.legra.ph/file/5a8e62a134991d1b57e69.jpg"
 
-@Client.on_message(filters.command(["start"]))
+photo = "https://te.legra.ph/file/834b1444f48d090886fef.jpg"
+
+@Client.on_message(filters.command(["start"], ["start@EndAfkBot"]) & filters.private)
 async def on_start(_, message: Message):
     bot_uptime = int(time.time() - boot)
     Uptime = get_readable_time(bot_uptime)
@@ -17,3 +19,24 @@ async def on_start(_, message: Message):
     await message.reply_photo(alpha,
         caption=f"hey {first_name} ! This is End Afk ! \n Nice to see ya here ✨💫! \n\n • Part of The End Network"
     )
+
+@Client.on_message(
+      filters.command(["start"], ["start@EndAfkBot"])
+    & filters.group
+    & ~filters.edited
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def omfoo(_, message: Message):
+   user = await _.get_me()
+   mention = user["mention"]
+   await _.send_photo(
+      message.chat.id,
+      photo,
+      caption=f"Hey! This is End Afk \n\n • part of The End network !")
+
+@Client.on_message(filters.command("ping"))
+async def ping(_, message: Message):
+    await _.send_message(
+       message.chat.id,
+       f"End is alive. \n\n Uptime - {Uptime}")
