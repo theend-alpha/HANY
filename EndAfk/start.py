4fc5d09a1632227037bae3fe5a6fc81651e68308
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-01 12:46:25 +0530

    Update start.py

--- EndAfk/start.py
+++ EndAfk/start.py
@@ -11,19 +11,19 @@ alpha = "https://te.legra.ph/file/5a8e62a134991d1b57e69.jpg"
 
 photo = "https://te.legra.ph/file/834b1444f48d090886fef.jpg"
 
-@Client.on_message(filters.command(["start"], ["start@EndAfkBot"]) & filters.private)
+@Client.on_message(filters.command(["start"], ["start@EndAfkBot"]))
 async def on_start(_, message: Message):
     bot_uptime = int(time.time() - boot)
-    global Uptime
     Uptime = get_readable_time(bot_uptime)
-    first_name = message.from_user.first_name
     await message.reply_photo(alpha,
-        caption=f"hey {first_name} ! This is End Afk ! \n Nice to see ya here ✨💫! \n\n • Part of The End Network"
+        caption=f"hey Client ! This is End Afk ! \n Nice to see ya here ✨💫! \n\n • Part of The End Network"
     )
 
 
 @Client.on_message(filters.command("ping") & filters.user(SUDOERS))
 async def ping(_, message: Message):
+    bot_uptime = int(time.time() - boot)
+    Uptime = get_readable_time(bot_uptime)
     await _.send_message(
        message.chat.id,
        f"End is alive. \n\n Uptime - {Uptime}")
