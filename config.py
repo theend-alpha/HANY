6010da2af1528a22cae81a68cf5cf488b7647810
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-05 00:35:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,27 @@
-from os import getenv
+import os
 
-from dotenv import load_dotenv
+ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
-load_dotenv()
-
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-
-BOT_TOKEN = getenv("BOT_TOKEN")
-
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
-) 
+if ENVIRONMENT:
+    try:
+        API_ID = int(os.environ.get('API_ID', 0))
+    except ValueError:
+        raise Exception("Your API_ID is not a valid integer.")
+    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    #if MUST_JOIN.startswith("@"):
+        #MUST_JOIN = MUST_JOIN.replace("@", "")
+else:
+    # Fill the Values
+    API_ID = 0
+    API_HASH = ""
+    BOT_TOKEN = ""
+    DATABASE_URL = ""
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
+    MUST_JOIN = ""
+    #if MUST_JOIN.startswith("@"):
+       # MUST_JOIN = MUST_JOIN[1:]
