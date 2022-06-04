bdb84e6889cd463c78171ebb0bb6b6416070b045
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-05 00:37:03 +0530

    Update start.py

--- HANY/start.py
+++ HANY/start.py
@@ -1,1 +1,17 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+from Yashvi import Keshav
 
+
+# Start Message
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+async def start(bot, msg):
+	user = await bot.get_me()
+	mention = user["mention"]
+	await bot.send_message(
+		msg.chat.id,
+		Data.START.format(msg.from_user.mention, mention),
+                disable_web_page_preview=True,
+		reply_markup=InlineKeyboardMarkup(Keshav.alpha_buttons)
+	)
