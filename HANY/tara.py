f0ba4bf9b4311505edb8051d2e2f9878d5b266d0
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-05 00:51:51 +0530

    Update tara.py

--- HANY/tara.py
+++ HANY/tara.py
@@ -2,15 +2,17 @@ from Alone import AlphaIsAlone
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, CallbackQuery
 
+F_ID = []
+
 alpha = "https://te.legra.ph/file/2154fc8d07f429b2eeef6.jpg"
 
 @Client.on_message(filters.command("tara") & filters.group & ~filters.forwarded & ~filters.via_bot)
 async def tara(_, message: Message):
-    global f_id
     global i_m
     global f_m
     i_id = message.from_user.id
     f_id = message.reply_to_message.from_user.id
+    F_ID.append(f_id)
     i_m = (await _.get_users(i_id)).mention
     f_m = (await _.get_users(f_id)).mention
     try:
@@ -23,7 +25,7 @@ async def tara(_, message: Message):
 @Client.on_callback_query()
 async def taraback(_: Client, query: CallbackQuery):
     resp_id = query.from_user.id
-    if resp_id == f_id:
+    if resp_id in F_ID:
         if query.data == "tara":
             try:
                 await query.message.reply_photo(alpha, AlphaIsAlone.TARAA.format(f_m, i_m))
