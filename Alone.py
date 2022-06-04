71033bf29840f2fa4b14d43eb4738ae76e848e3d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-05 00:32:10 +0530

    Create Alone.py

---
+++ Alone.py
@@ -0,0 +1,22 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class AlphaIsAlone:
+
+    #tara msg
+    TARA = """
+💗{} wants to kiss {}...
+"""
+
+    #tara buttons
+    tara_buttons = [ 
+          [  
+          InlineKeyboardButton(" ✅ ", callback_data="tara"),
+          InlineKeyboardButton(" ❌ ", callback_data="reject")
+          ]
+    ]
+
+    #tara accept
+    TARAA = """
+{} accepted the kiss of {} .
+"""
