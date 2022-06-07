d41b90fc224fde3c488688794c7b65748d63115f
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-07 16:18:26 +0530

    Update mygender.py

--- HANY/mygender.py
+++ HANY/mygender.py
@@ -10,29 +10,13 @@ OMFOO = []
 
 @Client.on_message(filters.command(["mygender", "mygender@nothehe_bot"]) & ~filters.via_bot & ~filters.forwarded & ~filters.edited)
 async def mygender(_, message: Message):
+    global i_id
     i_id = message.from_user.id
-    OMFOO.append(i_id)
     i_m = (await _.get_users(i_id)).mention
     c_id = message.chat.id
     await _.send_message(c_id, f"{i_m}, choose your gender", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.gender_markup))
 
-@Client.on_callback_query()
-async def genderback(_: Client, query: CallbackQuery):
-    q_id = query.from_user.id
-    if q_id in OMFOO:
-        if query.data == "male":
-            if q_id in FEMALES:
-                FEMALES.remove(q_id)
-            if q_id in MALES:
-                return
-            MALES.append(q_id)
-            await query.message.edit_text("your gender is updated to male 👦 ")
-            OMFOO.remove(q_id)
-        elif query.data == "female":
-            if q_id in MALES:
-                MALES.remove(q_id)
-            if q_id in FEMALES:
-                return
-            FEMALES.append(q_id)
-            await query.message.edit_text("your gender is updated to female 👧 ")
-            OMFOO.remove(q_id)
+@Client.on_callback_query(filters.regex("male"))
+async def Male(_: Client, query: CallbackQuery):
+    if query.from_user.id in OMFOO:
+        if 
