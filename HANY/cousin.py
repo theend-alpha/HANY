ad020710957eebf69e9f287f9a15079892f601d8
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-07 09:02:39 +0530

    Update cousin.py

--- HANY/cousin.py
+++ HANY/cousin.py
@@ -1,3 +1,5 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery
-from HANY.AlphaDB.cousins_adb import add_cousin, rmv_cousin
+from HANY.AlphaDB.cousins_adb import add_cousin, rmv_cousin, are_cousins
+
+
