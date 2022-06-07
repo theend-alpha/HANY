3a54bd566811151b34c7ef52bc812f9361fc05a1
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-07 09:35:14 +0530

    Update mygender.py

--- HANY/mygender.py
+++ HANY/mygender.py
@@ -1,1 +1,14 @@
-from 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, CallbackQuery, Message
+from Alone import AlphaIsAlone
+
+MALES = []
+
+FEMALES = []
+
+@Client.on_message(filters.command(["mygender", "mygender@nothehe_bot"]) & ~filters.via_bot & ~filters.forwarded & ~filters.edited)
+async def mygender(_, message: Message):
+    i_id = message.from_user.id
+    i_m = (await _.get_users(i_id)).mention
+    c_id = message.chat.id
+    await _.send_message(c_id, f"{i_m}, choose your gender", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.gender_markup))
