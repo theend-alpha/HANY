f8278561fbb0b78ff64151b4c72585e2b326023c
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-07 10:01:37 +0530

    Update mygender.py

--- HANY/mygender.py
+++ HANY/mygender.py
@@ -9,6 +9,7 @@ FEMALES = []
 @Client.on_message(filters.command(["mygender", "mygender@nothehe_bot"]) & ~filters.via_bot & ~filters.forwarded & ~filters.edited)
 async def mygender(_, message: Message):
     global i_id
+    global c_id
     i_id = message.from_user.id
     i_m = (await _.get_users(i_id)).mention
     c_id = message.chat.id
@@ -18,18 +19,11 @@ async def mygender(_, message: Message):
 async def genderback(_: Client, query: CallbackQuery):
     q_id = query.from_user.id
     msg_id = query.message.message_id
-    if q_id == i_id:
-        if query.data == "male":
+    if query.data == "male":
+        if q_id == i_id:
             if q_id in FEMALES:
                 FEMALES.remove(q_id)
             if q_id in MALES:
                 return
             MALES.append(q_id)
-            await query.message_edit_text("your gender is updated to male")
-        elif query.data == "female":
-            if q_id in MALES:
-                MALES.remove(q_id)
-            if q_id in FEMALES:
-                return
-            FEMALES.append(q_id)
-            await query.message_edit_text("your gender is updated to female")
+            await _.edit_message_text(chat_id=c_id, message_id=msg_id, text="your gender is updated to male")
