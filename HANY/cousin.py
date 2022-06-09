59c76280970d754c91b8b6b06a22b83bffac700c
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-09 15:42:07 +0530

    Update cousin.py

--- HANY/cousin.py
+++ HANY/cousin.py
@@ -19,10 +19,10 @@ async def csn(_, message: Message):
     id_is_male(i_id)
     id_is_female(i_id)
     if i_id in MALES:
-        await _.send_message(c_id, f"{i_m} wants {f_m} as his cousin...", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.cousin_buttons))
+        await _.send_message(c_id, f"{i_m} wants {f_m} as his cousin...", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.cousin_markup))
         MALES.remove(i_id)
     elif i_id in FEMALES:
-        await _.send_message(c_id, f"{i_m} wants {f_m} as her cousin...", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.cousin_buttons))
+        await _.send_message(c_id, f"{i_m} wants {f_m} as her cousin...", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.cousin_markup))
         FEMALES.remove(i_id)
     else:
-        await _.send_message(c_id, f"{i_m}, your gender is unspecified, please set it first !", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.set_gender_buttons))
+        await _.send_message(c_id, f"{i_m}, your gender is unspecified, please set it first !", reply_markup=InlineKeyboardMarkup(AlphaIsAlone.set_gender_markup))
