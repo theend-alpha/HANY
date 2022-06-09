37152f476bc93f7ebe92cd280355383da7a8afb3
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-09 10:06:19 +0530

    Update genders_adb.py

--- HANY/AlphaDB/genders_adb.py
+++ HANY/AlphaDB/genders_adb.py
@@ -35,7 +35,7 @@ async def add_male(i_id):
         SESSION.add(adder)
         SESSION.commit()
 
-async def add_female(i_id):
+def add_female(i_id):
     is_female = SESSION.query(Females).get(i_id)
     is_male = SESSION.query(Males).get(i_id)
     if is_female:
@@ -46,33 +46,28 @@ async def add_female(i_id):
         adder = Females(i_id)
         SESSION.add(adder)
         SESSION.commit()
-
-async def get_males():
+def get_males():
     try:
         return SESSION.query(Males).count()
     finally:
         SESSION.close()
-
-async def get_females():
+def get_females():
     try:
         return SESSION.query(Females).count()
     finally:
         SESSION.close()
-
-async def flee(i_id):
+def flee(i_id):
     is_male = SESSION.query(Males).get(i_id)
     is_female = SESSION.query(Females).get(i_id)
     if is_male:
         SESSION.delete(is_male)
     elif is_female:
         SESSION.delete(is_female)
-
-async def id_is_male(i_id):
+def id_is_male(i_id):
     is_male = SESSION.query(Males).get(i_id)
     if is_male:
         return True
-
-async def id_is_female(i_id):
+def id_is_female(i_id):
     is_female = SESSION.query(Females).get(i_id)
     if is_female:
         return True
