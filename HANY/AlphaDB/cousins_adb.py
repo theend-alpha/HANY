e6b22df8af6ed6547dbba1a5be702755a80a36de
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-11 11:48:28 +0530

    Update cousins_adb.py

--- HANY/AlphaDB/cousins_adb.py
+++ HANY/AlphaDB/cousins_adb.py
@@ -19,7 +19,7 @@ class Wait(BASE):
     i_id = Column(BigInteger, primary_key=True)
     f_id = Column(BigInteger, primary_key=True)
 
-    def __init__(self, i_id):
+    def __init__(self, i_id, f_id):
         self.i_id = i_id
         self.f_id = f_id
 
@@ -27,21 +27,21 @@ Cousins.__table__.create(checkfirst=True)
 
 Wait.__table__.create(checkfirst=True)
 
-def add_cousin(self, i_id, f_id):
+def add_cousin(i_id, f_id):
     if_c = SESSION.query(Cousins).get(i_id, f_id)
     if not if_c:
         one_c = Cousins(i_id, f_id)
         SESSION.add(one_c)
         SESSION.commit()
 
-def are_cousins(self, i_id, f_id):
+def are_cousins(i_id, f_id):
     try:
         SESSION.query(Cousins).get(i_id, f_id)
         cousins = i_id.f_id
     finally:
         SESSION.close()
 
-def rmv_cousin(self, i_id, f_id):
+def rmv_cousin(i_id, f_id):
     r_c = SESSION.query(Cousins).get(i_id, f_id)
     if r_c:
         SESSION.delete(r_c)
@@ -58,20 +58,20 @@ def cousins_list_for(i_id):
     finally:
         SESSION.close()
 
-def add_to_waiting(self, i_id, f_id):
+def add_to_waiting(i_id, f_id):
     in_waiting = SESSION.query(Wait).get(i_id, f_id)
     if not in_waiting:
         adder = Wait(i_id, f_id)
         SESSION.add(adder)
         SESSION.commit()
 
-def rmv_from_waiting(self, i_id, f_id):
+def rmv_from_waiting(i_id, f_id):
     in_waiting = SESSION.query(Wait).get(i_id, f_id)
     if in_waiting:
         SESSION.delete(in_waiting)
         SESSION.commit()  
 
-def check_waiting_list(self, i_id, f_id):
+def check_waiting_list(i_id, f_id):
     getter = SESSION.query(Wait).get(i_id, f_id)
     if getter:
         return True
