ce13e631ebff5ff15c88a7cead3063717b362bf5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 11:03:09 +0530

    Create cousins2.py

---
+++ HANY/AlphaDB/cousins2.py
@@ -0,0 +1,16 @@
+from YashviDB import db
+
+cousinsdb = db.cousinsdb
+
+
+async def are_cousins(i_id: int, f_id: int) -> bool:
+    cousins = await cousinsdb.find_one({"i_id": i_id}, {"f_id": f_id})
+    if cousins:
+        return True
+    else:
+        return False
+
+async def add_cousin(i_id: int, f_id: int):
+    cousins = await cousinsdb.find_one({"i_id": i_id}, {"f_id": f_id})
+    if not cousins:
+        await cousinsdb.insert_one({"i_id": i_id}, {"f_id": f_id})
