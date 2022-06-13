103776653dd9db564c982e5fd52dadd543c946be
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 16:38:14 +0530

    Update cousin.py

--- HANY/cousin.py
+++ HANY/cousin.py
@@ -83,7 +83,7 @@ async def hehe(_, message: Message):
         csn_msg = """"""
         omfoo = str(csn)
         YashuAlpha += f"{csn_m} ({omfoo})" + "\n"
-    csn_msg += "• " + YashuAlpha
+    csn_msg = "• " + YashuAlpha
     final_msg = f"Cousins for i_fn \n\n {csn_msg} \n "
     await _.send_message(c_id, final_msg)
 
